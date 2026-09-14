d3b5e0668648112ca5de4c05fa15ba7d971a65d4
Sahel <sahelmhd3@gmail.com>
2026-09-14 20:56:20 +0530

    Fix static files for Render deployment

--- backend/sulu_backend/settings.py
+++ backend/sulu_backend/settings.py
@@ -148,7 +148,7 @@ if CLOUDINARY_CONFIGURED:
             "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
         },
         "staticfiles": {
-            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage" if not DEBUG else "django.contrib.staticfiles.storage.StaticFilesStorage",
+            "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage" if not DEBUG else "django.contrib.staticfiles.storage.StaticFilesStorage",
         },
     }
 else:
