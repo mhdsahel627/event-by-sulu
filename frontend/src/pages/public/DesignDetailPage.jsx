bf65f59515dd04562bfca71d02a47aea97b0df95
Sahel <sahelmhd3@gmail.com>
2026-09-15 13:23:17 +0530

    Redesign admin management UI

--- frontend/src/pages/public/DesignDetailPage.jsx
+++ frontend/src/pages/public/DesignDetailPage.jsx
@@ -149,16 +149,7 @@ export default function DesignDetailPage() {
             {design.title}
           </h1>
 
-          <p
-            style={{
-              color: 'var(--text-secondary)',
-              fontSize: 'clamp(16px, 1.8vw, 19px)',
-              maxWidth: '850px',
-              lineHeight: 1.75,
-            }}
-          >
-            {design.short_description}
-          </p>
+
         </div>
 
         {/* Main Media Carousel Viewport */}
@@ -201,7 +192,7 @@ export default function DesignDetailPage() {
                 whiteSpace: 'pre-line',
               }}
             >
-              { design.detailed_description }
+              { design.short_description }
             </div>
 
             <div
